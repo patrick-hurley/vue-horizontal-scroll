df7d787ca873962680a626be07877ef6e01e8d5f
Patrick Hurley <patrick.hurley@indra.co.uk>
2024-02-05 11:45:04 +0000

    Project setup and component architecture

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import ScrollContainer from './components/ScrollContainer.vue'
+</script>
+
+<template>
+  <ScrollContainer>
+    <h1>Foo</h1>
+  </ScrollContainer>
+</template>
+
+<style scoped></style>
---
+++ src/components/ScrollContainer.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="scroll-container--wrapper">
+    <div class="scroll-container--content">
+      <slot></slot>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped></style>
